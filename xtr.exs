04ae70f891698f561f0c8f569f9e74822075eb6b
Wojciech Kaczmarek <wojtekk@kofeina.net>
2016-07-29 19:42:56 +0200

    real translator script

--- xtr.exs
+++ xtr.exs
@@ -5,13 +5,29 @@ Application.ensure_all_started :hound
 defmodule Xtr do
   use Hound.Helpers
 
-  def run do
+  def run(words) do
     Hound.start_session
-    navigate_to "http://translate.google.pl/#en/pl/foo"
-    IO.puts page_title
-
+    navigate_to "http://translate.google.pl/#en/pl/"
+    find_element(:id, "gt-otf-switch") |> click
+    s = find_element(:id, "source")
+    c = find_element(:id, "gt-submit")
+    r = find_element(:id, "result_box")
+    fill_field(s, words)
+    c |> click
+    wait_for_text(r, 50) |> IO.puts 
     Hound.end_session
   end
+
+  def wait_for_text(element, sleep_ms) do
+    result = inner_text(element) |> to_string
+    if result == "" do
+      :timer.sleep(sleep_ms)
+      wait_for_text(element, sleep_ms)
+    else
+      result
+    end
+  end
+
 end
 
-Xtr.run
+Xtr.run(System.argv |> Enum.join(" "))
